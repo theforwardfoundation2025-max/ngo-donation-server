6a77fc599b8d92e2ac1d465cb3a8ad2adc5c9738
Perlite53 <adityaubhat@gmail.com>
2026-06-16 15:20:20 +0530

    Fix Gmail port to 587

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app        = express();
 app.use(express.raw({ type: 'application/json' }));
 // Email transporter using Gmail
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
